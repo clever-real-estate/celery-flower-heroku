8b0436e5f6c9466a364dff45fe7f22b1d9481365
Nick Clucas <nick.clucas@movewithclever.com>
2025-07-25 21:04:03 -0400

    Update flowerconfig.py

--- flowerconfig.py
+++ flowerconfig.py
@@ -1,7 +1,6 @@
 import os
 
 auth_provider="flower.views.auth.GoogleAuth2LoginHandler"
-auth="allowed-emails.*@movewithclever.com"
 oauth2_key=os.getenv('GOOGLE_CLIENT_ID')
 oauth2_secret=os.getenv('GOOGLE_CLIENT_SECRET')
 oauth2_redirect_uri=os.getenv('GOOGLE_REDIRECT_URL')
